814e407fe950ff3398eb3e7561938ed0df3ef77c
Meena Mani <meenas.mailbag@gmail.com>
2019-02-20 09:05:16 -0800

    added val_split as an optional argument

--- make_directory.py
+++ make_directory.py
@@ -10,33 +10,41 @@ namely:
             class 1
 
 Author: Meena Mani
-Data: June 20 2018
+Date: Feb 20 2019
 
 """
 
 import os
 import numpy as np
 import shutil
-from glob import glob
 import random
+from glob import glob
 
 all_zero_files = glob('*/0/*.png', recursive=True)
 all_one_files = glob('*/1/*.png', recursive=True)
 
-def make_directory(classname):
-    
+def make_directory(classname, val_split = 0.2):
+"""
+    # Arguments
+        classname: Name of class -- either '0' or '1'.
+        val_split: Optional argument -- percent to split
+                   validation set. The default is 0.2. 
+"""
+
     if classname == '0':
         filenames = all_zero_files
     else:
         filenames = all_one_files
 
+    print('Building directory for class {}' .format(classname))
+
     for f in filenames:
         _, _, file =f.split('/')
         
-        if np.random.rand(1) < 0.2:
-            shutil.move(f, 'data/val/'+ classname + '/'+ file)
+        if np.random.rand(1) < val_split:
+            shutil.move(f, 'data/val/' + classname + '/' + file)
         else:
-            shutil.move(f, 'data/train/' + classname +'/'+ file) 
+            shutil.move(f, 'data/train/' + classname + '/' + file) 
 
 make_directory('0')
 make_directory('1')
